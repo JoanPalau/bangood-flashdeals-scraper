9d7afb00209780e1f1e58dfa57cad557a91650d3
JoanPalau <joanp4l4u@gmail.com>
2019-03-02 01:10:17 +0100

    Obtenció llistes correctes, no puc refactoritzar la funció atès al error find() takes no keyword arguments al parametritzar class_
    Signed-off-by: JoanPalau <joanp4l4u@gmail.com>

--- dealgetter.py
+++ dealgetter.py
@@ -16,12 +16,13 @@ class ClientWeb(object):
         html = self.descarregar_html(link)
 
         # buscar deals
+        results = self.buscar_deals(html)
 
         # imprimir resultat
         if path:
             self.store_result(path, html)
         else:
-            print(html)
+            print(results)
 
     def parse_arguments(self):
         parser = argparse.ArgumentParser(description='Returns flash offers from Bang good web.')
@@ -43,6 +44,57 @@ class ClientWeb(object):
         html = f.read()
         f.close()
         return html
+
+    def buscar_deals(self, html):
+        unbeatable_deals = []
+        result = []
+        arbre = bs4.BeautifulSoup(html, features='lxml')
+
+        results = self.get_li_data_product_info(arbre)
+
+        return results
+
+    def get_li_data_product_info(self, tree):
+        unbeatable_deals_class = tree.find_all(class_="unbeatabledealsitem newdealsgoods")
+        for deal_class in unbeatable_deals_class:
+            unbeatable_deals = deal_class.find_all('li')
+        results = []
+        for deal in unbeatable_deals:
+            result = []
+            # title = self.span_content_search(deal, class_='title')
+            # result.append(title)
+
+            title = []
+            spans = deal.find(class_='title')
+            for span in spans:
+                title = span.contents
+            result.append(title)
+
+            price = []
+            spans = deal.find(class_='price')
+            for span in spans:
+                price = span
+            result.append(price)
+
+            price = []
+            spans = deal.find(class_='price_old')
+            for span in spans:
+                price_old = span
+
+            result.append(price_old)
+
+            results.append(result)
+
+        return results
+
+
+    def span_content_search(self, parent, class_name):
+        for thing in parent:
+            spans = thing.find(class_name)
+            for span in spans:
+                content = span.contents
+            return content
+
 
 
 if __name__ == "__main__":
