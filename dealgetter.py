96a282a7f9861797bae166ec4eb3b743848e7d6e
JoanPalau <joanp4l4u@gmail.com>
2019-03-03 14:05:59 +0100

    Refactoritzades funcions obtenció dades, incorporat pprint
    Signed-off-by: JoanPalau <joanp4l4u@gmail.com>

--- dealgetter.py
+++ dealgetter.py
@@ -1,6 +1,8 @@
 from urllib.request import urlopen
+from pprint import pprint
 import bs4
 import argparse
+
 
 class ClientWeb(object):
     """"Client web per descarregar les ofertes de Bang good"""
@@ -20,9 +22,9 @@ class ClientWeb(object):
 
         # imprimir resultat
         if path:
-            self.store_result(path, html)
+            self.store_result(path, results)
         else:
-            print(results)
+            pprint(results)
 
     def parse_arguments(self):
         parser = argparse.ArgumentParser(description='Returns flash offers from Bang good web.')
@@ -36,7 +38,7 @@ class ClientWeb(object):
 
     def store_result(self, path, result):
         f = open(path, 'w')
-        f.write(str(result))
+        pprint(result, f)
         f.close()
 
     def descarregar_html(self, link):
@@ -46,55 +48,44 @@ class ClientWeb(object):
         return html
 
     def buscar_deals(self, html):
-        unbeatable_deals = []
         result = []
         arbre = bs4.BeautifulSoup(html, features='lxml')
 
-        results = self.get_li_data_product_info(arbre)
+        result.append(self.get_li_data_product_info(arbre, 'unbeatabledealsitem newdealsgoods'))
+        result.append(self.get_li_data_product_info(arbre, 'brandweekdealsbox newdealsgoods'))
 
-        return results
+        return result
 
-    def get_li_data_product_info(self, tree):
-        unbeatable_deals_class = tree.find_all(class_="unbeatabledealsitem newdealsgoods")
+    def get_li_data_product_info(self, tree, class_name):
+        results = []
+        unbeatable_deals_class = tree.find_all(class_=str(class_name))
         for deal_class in unbeatable_deals_class:
             unbeatable_deals = deal_class.find_all('li')
-        results = []
-        for deal in unbeatable_deals:
-            result = []
-            # title = self.span_content_search(deal, class_='title')
-            # result.append(title)
+            for deal in unbeatable_deals:
+                result = []
 
-            title = []
-            spans = deal.find(class_='title')
-            for span in spans:
-                title = span.contents
-            result.append(title)
+                title = []
+                spans = deal.find(class_='title')
+                for span in spans:
+                    title = span.contents[0]
+                result.append(title)
 
-            price = []
-            spans = deal.find(class_='price')
-            for span in spans:
-                price = span
-            result.append(price)
+                price = self.price_span_content_search(deal, 'price')
+                result.append(price)
 
-            price = []
-            spans = deal.find(class_='price_old')
-            for span in spans:
-                price_old = span
+                price_old = self.price_span_content_search(deal, 'price_old')
+                result.append(price_old)
 
-            result.append(price_old)
-
-            results.append(result)
+                results.append(result)
 
         return results
 
-
-    def span_content_search(self, parent, class_name):
-        for thing in parent:
-            spans = thing.find(class_name)
-            for span in spans:
-                content = span.contents
-            return content
-
+    def price_span_content_search(self, parent, class_name):
+        content = []
+        spans = parent.find(class_=str(class_name))
+        for span in spans:
+            content = span
+        return content
 
 
 if __name__ == "__main__":
